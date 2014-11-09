0920c0d1334852859dae9c55ae4492c4301696c5
Petr Klimeš <djpitrrs@gmail.com>
2014-11-09 16:28:15 +0100

    Creating Ajax presentrs

---
+++ Core/Presenters/AjaxPresenter.php
@@ -0,0 +1,47 @@
+<?php
+/**************************************************************
+ *
+ * AjaxPresenter.php, created 9.11.14
+ *
+ * Copyright (C) 2014 by Petr Klimes & development team
+ *
+ ***************************************************************
+ *
+ * Contacts: 
+ * @author: Petr Klimeš <djpitrrs@gmail.com>
+ * @url: http://www.pkrs.eu
+ * @url: https://github.com/pitrrs/PKRS
+ *
+ ***************************************************************
+ *
+ * Compatibility:
+ * PHP     v. 5.4 or higher
+ * MySQL   v. 5.5 or higher
+ * MariaDB v. 5.5 or higher
+ *
+ **************************************************************/
+namespace PKRS\Core\Presenters;
+
+abstract class AjaxPresenter extends BasePresenter{
+
+    var $json = array();
+
+    function need_login()
+    {
+
+    }
+
+    /**
+     * Not implemeted, but inheritance need this method
+     */
+    final function set_theme(){}
+
+    /**
+     * Exit before starting display
+     */
+    final function before_display(){
+        ob_clean();
+        echo json_encode($this->json);
+        exit;
+    }
+}
---
+++ Core/Presenters/IAjaxPresenter.php
@@ -0,0 +1,29 @@
+<?php
+/**************************************************************
+ *
+ * IAjaxPresenter.php, created 9.11.14
+ *
+ * Copyright (C) 2014 by Petr Klimes & development team
+ *
+ ***************************************************************
+ *
+ * Contacts: 
+ * @author: Petr Klimeš <djpitrrs@gmail.com>
+ * @url: http://www.pkrs.eu
+ * @url: https://github.com/pitrrs/PKRS
+ *
+ ***************************************************************
+ *
+ * Compatibility:
+ * PHP     v. 5.4 or higher
+ * MySQL   v. 5.5 or higher
+ * MariaDB v. 5.5 or higher
+ *
+ **************************************************************/
+namespace PKRS\Core\Presenters;
+
+interface IAjaxPresenter{
+
+    function need_login();
+
+}
